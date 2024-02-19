0d11d5ff659978a58c90d91ceeb035dbf18d6d85
RidwanHM <ridwan.h.mohamud@gmail.com>
2024-02-19 01:01:40 +0100

    push

---
+++ bin/pb/pb_migrations/1708300758_updated_vending_machines.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("sfh4b9czoxk0ptd")
+
+  collection.name = "tbane_stops"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("sfh4b9czoxk0ptd")
+
+  collection.name = "vending_machines"
+
+  return dao.saveCollection(collection)
+})
